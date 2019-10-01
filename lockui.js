e3bf5d1e26e6886283e9600fcf8e956c9a38a4c6
Tony Duran <yoet92@gmail.com>
2019-10-01 06:22:27 -0300

    npm

--- lockui.js
+++ lockui.js
@@ -1,21 +1,21 @@
-(function(){
+(function($w){
 
-    var lockHtmlParent = document.createElement('div');
+    var lockHtmlParent = $w.document.createElement('div');
         lockHtmlParent.classList.add('lockui-parent');
-    var lockHtml = document.createElement('div');
+    var lockHtml = $w.document.createElement('div');
         lockHtml.classList.add('lockui');
-    var lockHtmlContainer = document.createElement('div');
+    var lockHtmlContainer = $w.document.createElement('div');
         lockHtmlContainer.classList.add('lockui-container');
-    var lockStyle = document.createElement('style');
+    var lockStyle = $w.document.createElement('style');
         lockStyle.type = 'text/css';
         lockStyle.innerHTML = '.lockui-parent.is-unlocking>div{opacity:0;}.lockui{background-color:black;z-index: 50000;padding: 0px;border: none;height: 100%;opacity: 0.6;margin: 0px;width: 100%;left: 0px;top: 0px;cursor: wait;position: absolute;-webkit-transition: opacity .25s;-moz-transition: opacity .25s;-ms-transition: opacity .25s;-o-transition: opacity .25s;transition: opacity .25s;}.lockui-container{position: absolute;z-index: 50011;padding: 15px;margin: 0px;width: 30%;top: 40%;left: 35%;border: none;cursor: wait;opacity: 0.5;color: white;font-size: 11px;text-align: center;background-color: black;text-transform: uppercase;-webkit-border-radius: 10px;-moz-border-radius: 10px; border-radius: 10px;-webkit-transition: opacity .25s;-moz-transition: opacity .25s;-ms-transition: opacity .25s;-o-transition: opacity .25s;transition: opacity .25s;}';
         lockHtmlParent.appendChild(lockStyle);
         lockHtmlParent.appendChild(lockHtml);
         lockHtmlParent.appendChild(lockHtmlContainer);
 
-    window.lockUI = function(options) {
+    var lockUI = function(options) {
         options = options || {};
-        var parent = document.querySelector(options.selector) || document.body;
+        var parent = $w.document.querySelector(options.selector) || $w.document.body;
         if (parent === document.body) {
             lockHtml.style.position = 'fixed';
             lockHtmlContainer.style.position = 'fixed';
@@ -29,8 +29,8 @@
         parent.appendChild(lockHtmlParent);
     };
 
-    window.unlockUI = function() {
-        var element = document.querySelector('.lockui-parent');
+    var unlockUI = function() {
+        var element = $w.document.querySelector('.lockui-parent');
         if (element) {
             element.classList.add('is-unlocking');
             setTimeout(() => {
@@ -41,4 +41,16 @@
             }, 250);
         }
     };
-})();
+
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            module.exports = lockUI;
+            module.exports = unlockUI;
+        }
+        exports.lockUI = lockUI;
+        exports.unlockUI = unlockUI;
+    } else {
+        $w.lockUI = lockUI;
+        $w.unlockUI = unlockUI;
+    }
+})(this);
